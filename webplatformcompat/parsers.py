a762d1752bfb8c9d7d06325cf8293d58c4107c2d
John Whitlock <jwhitlock@mozilla.com>
2015-09-11 15:44:51 -0500

    bug 1153288 - Import drf-json-api code, step 1
    The v1 API uses JSON API release candidate 2 format, partially
    implemented by drf-json-api.  That code base is moving on to JSON API
    v1.0, so we'll need to preserve the old format until v1 goes away.
    
    Step 1 is copying the relevant code without much modification.  Coverage
    is no longer 100%, because a lot of the drf-json-api functionaility is
    unused or explicitly replaced.

--- webplatformcompat/parsers.py
+++ webplatformcompat/parsers.py
@@ -1,10 +1,90 @@
-from rest_framework_json_api.parsers import JsonApiParser \
-    as BaseJsonApiParser
-from rest_framework_json_api.parsers import JsonApiMixin
-from rest_framework_json_api.utils import snakecase
+"""webplatformcompat incoming data parsers"""
+#
+# rest_framework_json_api.parsers
+#
+from rest_framework import parsers, relations
+from .utils import model_from_obj, model_to_resource_type
+from django.utils import six
 
 
-class JsonApiParser(BaseJsonApiParser):
+class JsonApiMixin(object):
+    media_type = 'application/vnd.api+json'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        data = super(JsonApiMixin, self).parse(stream, media_type=media_type,
+                                               parser_context=parser_context)
+
+        view = parser_context.get("view", None)
+
+        model = self.model_from_obj(view)
+        resource_type = self.model_to_resource_type(model)
+
+        resource = {}
+
+        if resource_type in data:
+            resource = data[resource_type]
+
+        if isinstance(resource, list):
+            resource = [self.convert_resource(r, view) for r in resource]
+        else:
+            resource = self.convert_resource(resource, view)
+
+        return resource
+
+    def convert_resource(self, resource, view):
+        serializer_data = view.get_serializer(instance=None)
+        fields = serializer_data.fields
+
+        links = {}
+
+        if "links" in resource:
+            links = resource["links"]
+
+            del resource["links"]
+
+        for field_name, field in six.iteritems(fields):
+            if field_name not in links:
+                continue
+            if isinstance(field, relations.HyperlinkedRelatedField):
+
+                if field.many:
+                    pks = links[field_name]
+                    model = field.queryset.model
+
+                    resource[field_name] = []
+
+                    for pk in pks:
+                        obj = model(pk=pk)
+                        url = field.to_native(obj)
+
+                        resource[field_name].append(url)
+                else:
+                    pk = links[field_name]
+                    model = field.queryset.model
+
+                    obj = model(pk=pk)
+
+                    url = field.to_native(obj)
+
+                    resource[field_name] = url
+            else:
+                resource[field_name] = links[field_name]
+
+        return resource
+
+    def model_from_obj(self, obj):
+        return model_from_obj(obj)
+
+    def model_to_resource_type(self, model):
+        return model_to_resource_type(model)
+
+#
+# webplatformcompat implementation
+#
+from .utils import snakecase
+
+
+class JsonApiParser(JsonApiMixin, parsers.JSONParser):
     def model_to_resource_type(self, model):
         if model:
             return snakecase(model._meta.verbose_name_plural)
